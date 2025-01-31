521b7357145867935bc3394a1e0176211fb1a13f
rOyalFruit <ekdeon@gmail.com>
2025-01-31 17:31:46 +0900

    Feat: 리뷰 작성 기능 추가

--- backend/src/main/java/com/ll/nbe342team8/domain/book/review/controller/ReviewController.java
+++ backend/src/main/java/com/ll/nbe342team8/domain/book/review/controller/ReviewController.java
@@ -5,7 +5,10 @@ import com.ll.nbe342team8.domain.book.book.entity.Book;
 import com.ll.nbe342team8.domain.book.book.service.BookService;
 import com.ll.nbe342team8.domain.book.review.dto.ReviewResponseDto;
 import com.ll.nbe342team8.domain.book.review.entity.Review;
+import com.ll.nbe342team8.domain.book.review.repository.ReviewRepository;
 import com.ll.nbe342team8.domain.book.review.service.ReviewService;
+import com.ll.nbe342team8.domain.member.member.entity.Member;
+import com.ll.nbe342team8.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +22,7 @@ public class ReviewController {
 
     private final ReviewService reviewService;
     private final BookService bookService;
+    private final MemberService memberService;
 
     // 전체 리뷰 조회
     @GetMapping
@@ -28,6 +32,7 @@ public class ReviewController {
                 .map(ReviewResponseDto::from)
                 .collect(Collectors.toList());
     }
+
     // 특정 도서 리뷰 조회
     @GetMapping("/{book-id}")
     public List<ReviewResponseDto> getReviewsById(@PathVariable("book-id") Long bookId) {
@@ -43,11 +48,30 @@ public class ReviewController {
         reviewService.deleteReview(reviewId);
     }
 
-    @PostMapping("/{review-id}")
+    @PutMapping("/{review-id}")
     public void updateReview(@PathVariable("review-id") Long reviewId,
                              @RequestParam(name = "content") String content,
                              @RequestParam(name = "rating") float rating) {
 
         reviewService.updateReview(reviewId, content, rating);
+    }
+
+    @PostMapping("/{book-id}/{member-id}")
+    public void createReview(@PathVariable("book-id") Long bookId,
+                             @PathVariable("member-id") Long memberId,
+                             @RequestParam(name = "content") String content,
+                             @RequestParam(name = "rating") float rating){
+
+        Book book = bookService.getBookById(bookId);
+        Member member = memberService.getMemberById(memberId);
+
+        Review review = Review.builder()
+                .book(book)
+                .member(member)
+                .content(content)
+                .rating(rating)
+                .build();
+
+        reviewService.create(review);
     }
 }
