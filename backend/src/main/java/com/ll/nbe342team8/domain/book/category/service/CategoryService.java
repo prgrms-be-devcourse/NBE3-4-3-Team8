2cfb0e4eb191a1866045297d880106152d7953ba
Jay Lim <carneo.dev@gmail.com>
2025-02-11 10:57:37 +0900

    Revert "Feature: 관리자 페이지 도서, 로그인, 구문 관련 기능 추가 및 리팩토링"

---
+++ backend/src/main/java/com/ll/nbe342team8/domain/book/category/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.ll.nbe342team8.domain.book.category.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+}
