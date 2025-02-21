35c0b9908d6ab125290cecb1a40c78493505350d
1m1nkim <window12ty@naver.com>
2025-02-21 10:32:08 +0900

    Refactor: 코틀린 마이그레이션 - ReviewRepository 파일

--- backend/src/main/java/com/ll/nbe342team8/domain/book/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/ll/nbe342team8/domain/book/review/repository/ReviewRepository.java
@@ -1,15 +1,13 @@
 package com.ll.nbe342team8.domain.book.review.repository;
 
-import com.ll.nbe342team8.domain.book.book.entity.Book;
-import com.ll.nbe342team8.domain.book.review.entity.Review;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.ll.nbe342team8.domain.book.review.entity.Review
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.List
 
-import java.util.List;
+interface ReviewRepository : JpaRepository<Review, Long> {
+     fun findAllByBookId(bookId: Long) : List<Review>
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findAllByBookId(Long bookId);
-
-    Page<Review> findAllByBookId(Long bookId, Pageable pageable);
+     fun findAllByBookId(bookId: Long, pageable: Pageable): Page<Review>
 }
